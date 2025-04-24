2a2f1d41103b3ea152e60d6a0a85840470badf2d
dch9610 <dch9610@naver.com>
2025-04-24 10:52:51 +0900

    일정 크기 이하로 줄어들면 표시X 기능능

--- qms_web/lib/01.mainPage/main_screen.dart
+++ qms_web/lib/01.mainPage/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qms_web/01.mainPage/widgets/widget_day_production_status.dart';
 import 'package:qms_web/01.mainPage/widgets/widget_main_state.dart';
 import 'package:qms_web/util/util_color.dart';
 import 'package:qms_web/util/util_text.dart';
@@ -9,63 +10,89 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(title: Text("QMS")),
-      body: Row(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          WebNavWidget(currentIndex: 0),
+    return LayoutBuilder(
+      builder: (context, constrains) {
+        if (constrains.minWidth < 1000) {
+          return Scaffold(
+            body: Center(
+              child: Text(
+                "일정 크기 이하로 줄어들면 표시되지 않습니다!",
+                style: TextStyle(fontSize: 18, color: Colors.red),
+              ),
+            ),
+          );
+        }
+        return Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(title: Text("QMS")),
+          body: Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              WebNavWidget(currentIndex: 0),
 
-          Expanded(
-            child: Align(
-              alignment: AlignmentDirectional(0, -1),
-              child: Container(
-                width: double.infinity,
-                height: double.infinity,
-                constraints: BoxConstraints(maxWidth: 1500),
-                decoration: BoxDecoration(
-                  color: UtilityColor.primaryBackgroundColor,
-                ),
-                alignment: AlignmentDirectional(0, 0),
+              Expanded(
                 child: Align(
                   alignment: AlignmentDirectional(0, -1),
-                  child: Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(12, 0, 12, 0),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          SizedBox(height: 32),
-                          Text(
-                            "Dash board",
-                            style: UtilText.get20(
-                              context,
-                              UtilityColor.primaryTextColor,
-                            ).copyWith(fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(height: 5),
-                          Text(
-                            "Welcome to your company dashboard, review activity below.",
-                            style: UtilText.get11(
-                              context,
-                              UtilityColor.primaryTextColor,
-                            ).copyWith(fontWeight: FontWeight.bold),
-                          ),
+                  child: Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    constraints: BoxConstraints(maxWidth: 1500),
+                    decoration: BoxDecoration(
+                      color: UtilityColor.primaryBackgroundColor,
+                    ),
+                    alignment: AlignmentDirectional(0, 0),
+                    child: Align(
+                      alignment: AlignmentDirectional(0, -1),
+                      child: Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(12, 0, 12, 0),
+                        child: SingleChildScrollView(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              SizedBox(height: 32),
+                              Text(
+                                "Dash board",
+                                style: UtilText.get20(
+                                  context,
+                                  UtilityColor.primaryTextColor,
+                                ).copyWith(fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                "Welcome to your company dashboard, review activity below.",
+                                style: UtilText.get11(
+                                  context,
+                                  UtilityColor.primaryTextColor,
+                                ).copyWith(fontWeight: FontWeight.bold),
+                              ),
 
-                          WidgetMainDatacard(),
-                        ],
+                              WidgetMainDatacard(),
+
+                              const SizedBox(height: 20),
+
+                              Row(
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  WidgetDayProductionStatus(),
+
+                                  WidgetDayProductionStatus(),
+                                ],
+                              ),
+                              const SizedBox(height: 30),
+                            ],
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
